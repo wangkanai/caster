5bf01ab3379ccada72deb8c7f4bd3f9e1d93d5fc
wangkanai <sarin@wangkanai.com>
2025-04-15 16:23:14 +0700

    Add RTCM protocol classes to Domain layer
    Introduced `Rtcm`, `Rtcm2`, and `Rtcm3` classes under the `Protocals` namespace in the Domain layer. These additions provide a foundational structure for representing RTCM protocol types.

---
+++ src/Domain/Protocals/Rtcm.cs
@@ -0,0 +1,9 @@
+// Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.Apache License, Version 2.0
+
+namespace Wangkanai.Caster.Domain.Protocals;
+
+public class Rtcm { }
+
+public class Rtcm2 : Rtcm { }
+
+public class Rtcm3 : Rtcm { }
